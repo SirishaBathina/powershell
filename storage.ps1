a86ce311f33d6c0a5daffdcda95084c53526e174
SirishaBathina <141664760+SirishaBathina@users.noreply.github.com>
2023-08-07 21:07:54 +0100

    Update storage.ps1

--- storage.ps1
+++ storage.ps1
@@ -2,3 +2,11 @@
  -Name addstoragesiri `
  -ResourceGroupName myResourceGroup `
   -TemplateFile $templateFile
+
+
+  $templateFile = "C:\Users\devop\ITC\ARM\template1.json"
+PS C:\Users\devop> New-AzResourceGroupDeployment `
+>>   -Name addnameparameter `
+>>   -ResourceGroupName myResourceGroup `
+>>   -TemplateFile $templateFile `
+>>   -storageName bsiri
